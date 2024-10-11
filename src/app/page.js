a7443a6a86104c181d28d63dcb6a01564aa4bc3c
Atul Kumar <atulkumar.dev@protonmail.com>
2024-10-11 17:09:39 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -3,41 +3,67 @@ import { Button } from "@chakra-ui/react";
 export default function Home() {
   return (
     <>
-      <div className=" flex relative">
-        <Image
-          src="/pizza-image.jpg"
-          alt="pizza image"
-          className=" absolute -mt-5 right-0 rounded-bl-[140px] z-10"
-          width={400}
-          height={350}
-        />
-        <Image
-          src="/vector.png"
-          alt="vector art"
-          className=" right-0 absolute z-20"
-          width={400}
-          height={370}
-        />
-        <Button
-          display="flex"
-          _hover={{
-            background: "white",
-            color: "red.500",
-          }}
-          pos="absolute"
-          right="0"
-          zIndex={30}
-          color="white"
-          rounded="full"
-          mt="4"
-          mr="4"
-          variant="outline"
-        >
-          Get In Touch
-        </Button>
-        <div>Discover the Best Food and Drinks</div>
+      <div className=" flex flex-col">
+        <div>
+          <Image
+            src="/pizza-image.jpg"
+            alt="pizza image"
+            className=" absolute -mt-5 right-0 rounded-bl-[140px] z-10"
+            width={420}
+            height={350}
+          />
+          <Image
+            src="/vector.png"
+            alt="vector art"
+            className=" right-0 absolute z-20"
+            width={420}
+            height={370}
+          />
+          <Button
+            display="flex"
+            _hover={{
+              background: "white",
+              color: "red.500",
+            }}
+            pos="absolute"
+            right="0"
+            zIndex={30}
+            color="white"
+            rounded="full"
+            mt="4"
+            mr="4"
+            variant="outline"
+          >
+            Get In Touch
+          </Button>
+        </div>
+
+        {/* This section contains a heading, a description, and a button for exploring more */}
+        <div>
+          <div className=" font-bold text-3xl text-[#0E2368]">
+            Discover the <span className=" text-[#E23744]">Best</span> Food and
+            Drinks
+          </div>
+          <div className=" text-[#444957 ]">
+            Naturally made Healthcare Products for the better care & support of
+            your body.
+          </div>
+          <Button
+            bg="#E23744"
+            _hover={{
+              background: "red.600",
+            }}
+            color="white"
+            rounded="full"
+          >
+            Explore Now!
+          </Button>
+        </div>
+
+        {/* .................. */}
+
+
       </div>
-      
     </>
   );
 }
